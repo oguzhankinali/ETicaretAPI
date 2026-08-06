d829f8877f80cc66be9b057b67e680ae0c9a44d8
Oguzhan Kinali <oguzhankinali07@gmail.com>
2026-08-06 23:35:15 +0300

    feat: implement concrete repositories and register dependency injections

--- Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
+++ Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Repositories
 {
-    public interface IReadRepository<T> : IRepository<T> where T: class
+    public interface IReadRepository<T> : IRepository<T> where T: BaseEntity
     {
         IQueryable<T> GetAll();
         IQueryable<T> GetWhere(Expression<Func<T,bool>> method);
-        T GetSingle(Expression<Func<T, bool>> method);
-        T GetById(string id);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method);
+        Task<T> GetByIdAsync(string id);
     }
 }
