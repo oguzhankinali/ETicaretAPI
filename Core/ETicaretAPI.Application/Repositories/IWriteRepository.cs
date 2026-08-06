acf89a6b3bf391dbae04c37efaf057dab13e5e37
Oguzhan Kinali <oguzhankinali07@gmail.com>
2026-08-06 21:40:01 +0300

    feat: initial onion architecture setup and repository interfaces

---
+++ Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T: class
+    {
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using ETicaretAPI.Persistance.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ETicaretAPIDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("PostgreSQL"));
+        });
+        }
+    }   
+}
