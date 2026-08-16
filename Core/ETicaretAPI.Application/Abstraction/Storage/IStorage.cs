0042381b4b1f1e77fd26972c85d45b3a93ac3938
Oguzhan Kinali <oguzhankinali07@gmail.com>
2026-08-16 03:42:40 +0300

    feat: completed IStorage interface contract with upload, delete, getfiles, and hasfile methods

---
+++ Core/ETicaretAPI.Application/Abstraction/Storage/IStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ETicaretAPI.Application.Abstraction.Storage
+{
+    public interface IStorage
+    {
+        Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string pathOrContainerName, IFormFileCollection files);
+
+        Task DeleteAsync(string pathOrContainerName, string fileName);
+    }
+}
