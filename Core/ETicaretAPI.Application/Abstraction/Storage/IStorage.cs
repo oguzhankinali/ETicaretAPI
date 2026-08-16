50e97435bb13a86a7be2a62aead294bb0f5a25ea
Oguzhan Kinali <oguzhankinali07@gmail.com>
2026-08-16 22:57:00 +0300

    feat infrastructure: implement LocalStorage service with file operations

--- Core/ETicaretAPI.Application/Abstraction/Storage/IStorage.cs
+++ Core/ETicaretAPI.Application/Abstraction/Storage/IStorage.cs
@@ -12,5 +12,9 @@ namespace ETicaretAPI.Application.Abstraction.Storage
         Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string pathOrContainerName, IFormFileCollection files);
 
         Task DeleteAsync(string pathOrContainerName, string fileName);
+
+        List<string> GetFiles(string pathOrContainerName);
+
+        bool HasFile(string pathOrContainerName, string fileName);
     }
 }
---
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/Local/LocalStorage.cs
@@ -0,0 +1,46 @@
+﻿using ETicaretAPI.Application.Abstraction.Storage;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Infrastructure.Services.Storage.Local
+{
+    public class LocalStorage : ILocalStorage
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public LocalStorage(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public Task DeleteAsync(string pathOrContainerName, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetFiles(string pathOrContainerName)
+        {
+            string filePath = Path.Combine(_webHostEnvironment.WebRootPath, pathOrContainerName);
+            DirectoryInfo directoryInfo = new DirectoryInfo(filePath);
+            List<string> files = directoryInfo.GetFiles().Select(f=>f.Name).ToList();
+            return files;
+        }
+
+        public bool HasFile(string pathOrContainerName, string fileName)
+        {
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, pathOrContainerName, fileName);
+            return File.Exists(path);
+            
+        }
+
+        public Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string pathOrContainerName, IFormFileCollection files)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
